ba43d435d41308dc1e5072dd3db38a7f70472423
KimmengHaoTSC <kimmeng.hao@techostartup.center>
2022-06-16 08:42:55 +0700

    edit on Footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -11,35 +11,41 @@
                         <v-row>Phone: (855) 81 888 296<br/>
                             Email: camdx@mef.gov.kh</v-row>
                     </v-col>
-                    <v-col cols="12" md="3" sm="6" xs="12">
+                    <v-col v-if="$route.path === '/' || $route.path == '/kh'" cols="12" md="3" sm="6" xs="12">
                         <v-row class="mb-5">
+                            <v-icon>mdi-chevron-right</v-icon> 
                             <a @click="$vuetify.goTo('#overview')">
                                 CamDX
                             </a>
                         </v-row>
                         <v-row class="mb-5">
+                            <v-icon>mdi-chevron-right</v-icon> 
                             <a @click="$vuetify.goTo('#principles')">
                                 Principles
                             </a>
                         </v-row>
                         <v-row class="mb-5">
+                            <v-icon>mdi-chevron-right</v-icon> 
                             <a @click="$vuetify.goTo('#camdigikey')">
                                 CamDigiKey
                             </a>
                         </v-row>
                     </v-col>
-                    <v-col cols="12" md="2" sm="6" xs="12">
+                    <v-col v-if="$route.path === '/' || $route.path == '/kh'" cols="12" md="2" sm="6" xs="12">
                         <v-row class="mb-5">
+                            <v-icon>mdi-chevron-right</v-icon> 
                             <a @click="$vuetify.goTo('#service')">
                                 Services
                             </a>
                         </v-row>
                         <v-row class="mb-5">
+                            <v-icon>mdi-chevron-right</v-icon> 
                             <a @click="$vuetify.goTo('#faq')">
                                 F.A.Q
                             </a>
                         </v-row>
                         <v-row class="mb-5">
+                            <v-icon>mdi-chevron-right</v-icon> 
                             <a @click="$vuetify.goTo('#contact')">
                                 Contact Us
                             </a>
@@ -54,15 +60,12 @@
                 </v-row>
             </v-col>
             <v-divider></v-divider>
-            <v-row class="ma-4">
-                <v-col col="4" >
-                    <v-list-item-title class="textSize">©2020-2022 CamDX. All Rights Reserved</v-list-item-title>
+            <v-row class="ma-4" >
+                <v-col cols="12" md="6" sm="12" xs="12">
+                    <div class="textSize">©2020-2022 CamDX. All Rights Reserved</div>
                 </v-col>
-                <v-col col="4">
-
-                </v-col>
-                <v-col col="4">
-                    <v-list-item-title class="textSize">Terms of Use | Privacy Policy</v-list-item-title>
+                <v-col cols="12" md="6" sm="12" xs="12" align="right" >
+                    <div class="textSize">Terms of Use | Privacy Policy</div>
 
                 </v-col>
             </v-row>
